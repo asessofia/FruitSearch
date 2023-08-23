aa7d9bd19f80c793b08a9960995bd27833298c88
Abel Sessofia <asessofia@twochairs.com>
2023-08-23 11:41:19 -0500

    adding first commits

---
+++ script.js
@@ -0,0 +1,77 @@
+const input = document.querySelector('#fruit');
+const suggestions = document.querySelector('.suggestions ul');
+
+const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
+
+input.addEventListener('focus', function() {
+	if (this.value === '') {
+	  this.placeholder = 'Search fruit 🍎';
+	}
+  });
+  
+  input.addEventListener('blur', function() {
+	this.placeholder = '';
+  });
+
+function search(str) {
+	let results = [];
+
+	if (str.length > 0) {
+		results = fruit.filter(fruit => fruit.toLowerCase().includes(str.toLowerCase()));
+	  }
+
+   return results;
+  
+}
+
+function searchHandler(e) {
+	const inputVal = e.target.value;
+	const results = search(inputVal);
+
+	showSuggestions(results, inputVal);
+}
+
+function showSuggestions(results, inputVal) {
+	suggestions.innerHTML = '';
+
+  if (results.length > 0) {
+    results.forEach(result => {
+      const li = document.createElement('li');
+      li.textContent = result;
+      suggestions.appendChild(li);
+    });
+  } else {
+    const li = document.createElement('li');
+    li.textContent = `No suggestions for "${inputVal}"`;
+    suggestions.appendChild(li);
+  }
+
+}
+
+function useSuggestion(e) {
+	const clickedText = e.target.textContent;
+
+  if (clickedText) {
+    input.value = clickedText;
+    suggestions.innerHTML = '';
+  }
+	
+}
+
+function highlightSuggestion(e) {
+  const hoveredText = e.target.textContent;
+
+  if (hoveredText) {
+    const listItems = suggestions.getElementsByTagName('li');
+
+    Array.from(listItems).forEach(item => {
+      item.style.backgroundColor = '';
+    });
+
+    e.target.style.backgroundColor = 'yellow';
+  }
+}
+
+input.addEventListener('keyup', searchHandler);
+suggestions.addEventListener('click', useSuggestion);
+suggestions.addEventListener('mouseover', highlightSuggestion);
